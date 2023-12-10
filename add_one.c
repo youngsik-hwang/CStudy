a37781e3d30e81f92904eda9b2180f573ffaeb81
youngsik-hwang <youngisman@gmail.com>
2023-12-10 11:13:03 +0900

    첫번째 c 소스

---
+++ add_one.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void){
+	int ultra = 0, super = 0;
+	while(super < 50){
+		super++;
+		++ultra;
+		printf("super = %d, ultra = %d\n", super, ultra);
+	
+	}
+	
+	return 0;
+}
+
